6f8f215c81545913dee18b4399cc86a5607e8d35
Álvaro <alvaro130303@gmail.com>
2026-03-16 17:32:03 +0100

    imprimir decimal en binario y hexadecimal

--- src/main.asm
+++ src/main.asm
@@ -1,21 +1,26 @@
-.data
-numero: .word 45
-numero2: .word 15
-resultado: .word 0
+.data                   # seccion de datos
+numero: .word 3         # crea variable numero y la inicializa en 3
 
-.text
-.globl main
+.text                   # seccion de codigo
+.globl main             # indica que globl (inicio del programa), es la etiqueta main
 
-main:
-    lw t0, numero
-    lw t1, numero2
+main:                   # etiqueta main
 
-    add t2, t0, t1
-    mv a0, t2
+    lw t0, numero       # carga en t0, lo que haya en numero
+    mv a0, t0           # mueve a a0, lo que haya en t0
 
-    li a7, 1
-    ecall
+    li a7, 34           # mete en a7, el valor 34
+    ecall               # ejecuta la instruccion de a7, en este caso la 34 (imprimir hexadecimal)
 
-    li a7, 10
-    ecall
+    li a0, 10           # mete en a0 un 10
+    li a7, 11           # mete en a7 un 11
+    ecall               # ejecuta la instruccion de a7, en este caso la 11 (imprimir caracter), 10 en ascii es salto de linea
 
+    mv a0, t0           # mueve a a0, lo que haya en t0
+    li a7, 35           # mete en a7, el valor 35
+    ecall               # ejecuta la instruccion de a7, en este caso la 34 (imprimir binario)
+
+
+salir:
+    li a7, 10           # mete en a7, el valor 10
+    ecall               # ejecuta la instruccion de a7, en este caso la 10 (finalizar programa)
